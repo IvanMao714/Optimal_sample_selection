7e946b889cf1b1b1a773f880cd008a52f637a10a
Ivan <maoyifan714@gmail.com>
2023-03-26 07:22:11 +0800

    upload combin algorithm

--- algorthm/test_1.2.2.py
+++ algorthm/test_1.2.2.py
@@ -92,7 +92,7 @@ def search(m, n, k, j, s):
     # choosen_letter.sort()
 
     # choosen_letter = list(x for x in range(n)) # test
-    choosen_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G']  # test
+    choosen_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # test
     print(choosen_letter)
 
     combinations = combine(choosen_letter, n, k, s)
@@ -102,4 +102,4 @@ def search(m, n, k, j, s):
 
 if __name__ == '__main__':
     # search(45, 10, 6, 6, 4)
-    search(45, 7, 6, 5, 5)
+    search(45, 8, 6, 4, 4)
